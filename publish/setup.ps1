36e86a6fbd62fe3af4c7753ade08f1f91d92bd13
Ed_ <edwardgz@gmail.com>
2026-08-25 13:55:15 -0400

    diff theme..

--- publish/setup.ps1
+++ publish/setup.ps1
@@ -10,4 +10,4 @@ Copy-Item -LiteralPath $config -Destination (Join-Path $site "quartz.config.yaml
 Set-Location -LiteralPath $site
 npm ci
 npx quartz plugin install --from-config
-npm install @quartz-themes/shimmering-focus
+npm install @quartz-themes/everforest-spruce
